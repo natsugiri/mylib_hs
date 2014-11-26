79b5701b9b16bf0e056f1258fc1147d77712520e
tomohiro <natsugiri@gmail.com>
2014-11-26 15:21:12 +0900

    add Queue.hs

---
+++ DataStructure/Queue.hs
@@ -0,0 +1,11 @@
+type Qu = Seq.Seq
+
+emptyQu = Seq.empty
+
+pushQu :: a -> Qu a -> Qu a
+pushQu a qu = a <| qu
+
+popQu :: Qu a -> (a, Qu a)
+popQu qu =
+    let (qu1 :> a) = viewr qu
+    in (a, qu1)
