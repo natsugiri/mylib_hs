40e4ec17baef3a00f52627479657d1e1804b7640
natsugiri <natsugiri@gmail.com>
2014-11-26 23:46:33 +0900

    vecotr unboxed

--- Common/Template.hs
+++ Common/Template.hs
@@ -7,8 +7,11 @@ import Data.Function (on)
 import Data.Char
 
 import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as U
 import qualified Data.Sequence as Seq
 import Data.Sequence (viewl, viewr, ViewL((:<)), ViewR((:>)))
+
+import Data.Graph
 
 getInt :: IO Int
 getInt = read <$> getLine
@@ -19,4 +22,6 @@ getInts = map read .words <$> getLine
 main = do
   l <- getInts
   print l
+  let g = buildG (0, 3) [(0, 1), (1, 2), (2, 3), (3, 1), (0, 2), (0, 2)]
+  print g
 
